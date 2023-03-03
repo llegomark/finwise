11d32ecf28dc5a6f2c5ac25879e070ccfdfa5d74
Mark Anthony Llego <lr.mallego@mmdc.mcl.edu.ph>
2023-03-04 05:51:23 +0800

    updates

--- pages/index.tsx
+++ pages/index.tsx
@@ -115,7 +115,7 @@ const Home: NextPage = () => {
       const chunkValue = decoder.decode(value);
       setGeneratedTopics((prev) => prev + chunkValue);
     }
-    scrollToFinguidance(); // Scroll to the generated passage
+    scrollToFinguidance(); // Scroll to the generated financial guidance section
     setLoading(false); // Set the loading state to false once the response is fully received
   };
 
@@ -158,66 +158,84 @@ const Home: NextPage = () => {
           <SocialIcon platform="github" size={25} />
           <p>Star on Github</p>
         </a>
-        <h2 className="mx-auto max-w-4xl text-5xl font-bold tracking-normal text-slate-900 sm:text-6xl md:text-7xl">
-          <Balancer>
-            Get AI-Powered Guidance on Debt and Financial Management Today
-          </Balancer>
-        </h2>
-        <p
-          className="mx-auto mt-6 max-w-xl text-base leading-normal text-slate-900 sm:mt-12 sm:text-lg lg:text-lg"
-          aria-label="Reading Passages"
-        >
-          <Balancer>
-            Say goodbye to financial stress and take control of your money with
-            our AI-powered guidance. Whether you&apos;re struggling with debt or
-            looking to improve your overall financial health, our platform
-            provides personalized advice to help you achieve your goals.
-          </Balancer>
-        </p>
-        <div className="w-full max-w-xl px-6">
-          <div className="align-items-center mt-10 flex items-center">
-            <span className="leading-zero flex h-6 w-6 items-center justify-center rounded-full bg-black p-2 text-center text-white">
-              1
-            </span>
-            <p className="ml-3 text-left text-base leading-normal text-slate-900 sm:text-lg lg:text-lg">
+        <div className="mx-auto max-w-4xl px-4 sm:px-6 lg:px-8">
+          <h2 className="text-center text-5xl font-bold tracking-normal text-slate-900 sm:text-6xl md:text-7xl">
+            <Balancer>
+              Start Your Journey Towards Financial Freedom and Peace of Mind
+              Today!
+            </Balancer>
+          </h2>
+          <div className="mx-auto max-w-3xl">
+            <p
+              className="my-6 text-base leading-6 text-gray-500 sm:text-lg sm:leading-7 lg:text-lg lg:leading-8"
+              aria-label="Financial Guidance"
+            >
               <Balancer>
-                Tell us about your financial concerns or questions.
+                Are you struggling to manage your debts and financial
+                obligations? Do you feel overwhelmed by the complexities of the
+                financial world? It&apos;s time to take charge of your finances
+                and make informed decisions with the help of Artificial
+                Intelligence (AI)-powered guidance.
+              </Balancer>
+            </p>
+            <p
+              className="mt-6 text-base leading-6 text-gray-500 sm:text-lg sm:leading-7 lg:text-lg lg:leading-8"
+              aria-label="Financial Guidance"
+            >
+              <Balancer>
+                Our cutting-edge platform offers personalized and comprehensive
+                advice on debt management, financial planning, and investment
+                strategies. With just a few clicks, you can access a wealth of
+                information and insights that will help you achieve your
+                financial goals and secure your future.
               </Balancer>
             </p>
           </div>
-          <form onSubmit={handleSubmit}>
-            <textarea
-              value={topic}
-              onChange={(e) => setTopic(e.target.value)}
-              onInput={limitCharacters}
-              rows={4}
-              className="focus:shadow-outline mt-5 w-full rounded-lg bg-neutral-50 shadow-sm focus:outline-none"
-              placeholder={
-                "Are you facing challenges with debt repayment due to unfair debt collection practices by financing and lending companies? Are you trying to save for a big purchase while also managing your debt? Or are you simply seeking general financial advice? Our AI-powered platform is here to assist you. Describe your financial situation and objectives, and our technology will provide customized guidance specifically designed to meet your individual needs, including addressing unfair debt collection practices by financing and lending companies."
-              }
-              aria-label="Enter a theme, subject matter, or content focus. (Leave blank to generate a random passage.)"
-            />
-            <p className="mb-1 text-right text-sm text-gray-500">
-              {topic.length}/400
-            </p>
-            {!loading && (
-              <button
-                className="w-full rounded-lg bg-black px-4 py-2 text-base font-bold text-white transition-colors hover:bg-black/80"
-                type="submit"
-                disabled={topic === ""}
-              >
-                Get Financial Guidance &rarr;
-              </button>
-            )}
-            {loading && (
-              <button
-                className="w-full rounded-lg bg-black px-4 py-2 text-base font-bold text-white"
-                disabled
-              >
-                Typing...
-              </button>
-            )}
-          </form>
+          <div className="mx-auto mt-10 w-full max-w-xl px-4">
+            <div className="flex items-center">
+              <span className="leading-zero flex h-6 w-6 items-center justify-center rounded-full bg-black p-2 text-center text-white">
+                1
+              </span>
+              <p className="ml-3 text-left text-base leading-normal text-slate-900 sm:text-lg lg:text-lg">
+                <Balancer>
+                  Tell us about your financial concerns or questions.
+                </Balancer>
+              </p>
+            </div>
+            <form onSubmit={handleSubmit} className="mt-5">
+              <textarea
+                value={topic}
+                onChange={(e) => setTopic(e.target.value)}
+                onInput={limitCharacters}
+                rows={4}
+                className="focus:shadow-outline w-full rounded-lg bg-neutral-50 text-base leading-6 text-gray-500 shadow-sm focus:outline-none sm:text-lg lg:text-lg"
+                placeholder={
+                  "Are you facing challenges with debt repayment due to unfair debt collection practices by financing and lending companies? Are you trying to save for a big purchase while also managing your debt? Or are you simply seeking general financial advice? Our AI-powered platform is here to assist you. Describe your financial situation and objectives, and our technology will provide customized guidance specifically designed to meet your individual needs, including addressing unfair debt collection practices by financing and lending companies."
+                }
+                aria-label="Enter a theme, subject matter, or content focus. (Leave blank to generate a random passage.)"
+              />
+              <p className="mb-1 text-right text-sm text-gray-500">
+                {topic.length}/400
+              </p>
+              {!loading && (
+                <button
+                  className="w-full rounded-lg bg-black px-4 py-2 text-base font-bold text-white transition-colors hover:bg-black/80"
+                  type="submit"
+                  disabled={topic === ""}
+                >
+                  Get Financial Guidance &rarr;
+                </button>
+              )}
+              {loading && (
+                <button
+                  className="w-full rounded-lg bg-black px-4 py-2 text-base font-bold text-white"
+                  disabled
+                >
+                  Typing...
+                </button>
+              )}
+            </form>
+          </div>
         </div>
         <Toaster
           position="bottom-right"
@@ -243,10 +261,7 @@ const Home: NextPage = () => {
                         navigator.clipboard
                           .writeText(financialguidance)
                           .then(() => {
-                            toast.success(
-                              "AI-Powered Financial Guidance Copied",
-                              {}
-                            );
+                            toast.success("Financial Guidance Copied", {});
                           })
                           .catch((error) => {
                             console.error(error);
@@ -256,6 +271,7 @@ const Home: NextPage = () => {
                       <p
                         className="text-start text-base leading-normal text-slate-900 sm:text-lg lg:text-lg"
                         ref={finguidanceRef}
+                        aria-label="Financial Guidance"
                       >
                         {lines.map((line, index) => (
                           <React.Fragment key={index}>
